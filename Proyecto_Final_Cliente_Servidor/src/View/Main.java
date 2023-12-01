eeb1fc300f4e71327f74e7c80913c3ad26cb1a28
Sebastian <Sebawtisn@gmail.com>
2023-11-30 22:21:59 -0600

    Prueba1

---
+++ Proyecto_Final_Cliente_Servidor/src/View/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package View;
+public class Main {
+    public static void main(String[] args) {
+        GuiPrinci Gui = new GuiPrinci();
+        Gui.setVisible(true);
+    }
+    
+}
